7e1fa846f941acd4dbf17ee1cfc566e000dafefa
oxniu <oxniu@users.noreply.github.com>
2018-08-25 19:02:38 +0800

    Update SearchBT.php

--- lib/SearchBT.php
+++ lib/SearchBT.php
@@ -43,8 +43,8 @@ class SearchBT
         $re=str_replace(["\r","\n","<b>","</b>"],"",$re);
         $pattern="/<dl class='item'>.*?target='_blank'>(.*?)<.*?收录时间:(.*?)<.*?(magnet:.*?)' >/";
         preg_match_all($pattern,$re,$matches);
-//        var_dump($matches);
-        print($matches);
+        var_dump($matches);
+        
         $items=array();
         for ($i=0;$i<count($matches[0]);$i++){
             $item=new \stdClass();
