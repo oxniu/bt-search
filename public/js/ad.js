07ef1cb12256af7b15e925f452e1f4c2116b4b95
oxniu <oxniu@users.noreply.github.com>
2018-08-26 20:57:23 +0800

    Update ad.js

--- public/js/ad.js
+++ public/js/ad.js
@@ -1,7 +1,78 @@
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<script>
-  (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: "ca-pub-8638221760401486",
-    enable_page_level_ads: true
-  });
+<script type="text/javascript"> 
+//default pop-under house ad url 
+clicksor_enable_pop = true; 
+clicksor_adhere_opt='left'; 
+clicksor_frequencyCap =0.1;
+durl = '';
+clicksor_layer_border_color = '';
+clicksor_layer_ad_bg = '';
+clicksor_layer_ad_link_color = '';
+clicksor_layer_ad_text_color = '';
+clicksor_text_link_bg = '';
+clicksor_text_link_color = '';
+clicksor_enable_inter=true;
 </script>
+ <script type="text/javascript" src="http://b.clicksor.net/show.php?nid=1&amp;pid=392679&amp;sid=655850"></script>
+
+<script type="text/javascript"> 
+clicksor_adhere_opt='right'; 
+
+clicksor_default_url = '';
+clicksor_banner_border = '#99CC33'; 
+clicksor_banner_ad_bg = '#FFFFFF';
+clicksor_banner_link_color = '#000000'; 
+clicksor_banner_text_color = '#666666';
+clicksor_layer_border_color = '';
+clicksor_layer_ad_bg = ''; 
+clicksor_layer_ad_link_color = '';
+clicksor_layer_ad_text_color = ''; 
+clicksor_text_link_bg = '';
+clicksor_text_link_color = ''; 
+clicksor_enable_text_link = false;
+		 
+clicksor_banner_text_banner = true;
+clicksor_banner_image_banner = true; 
+clicksor_enable_layer_pop = false;
+clicksor_enable_pop = true;
+</script>
+ <script type="text/javascript" src="http://b.clicksor.net/show.php?nid=1&amp;pid=392679&amp;adtype=6&amp;sid=655850"></script>
+
+<script type="text/javascript"> 
+clicksor_adhere_opt='right'; 
+
+clicksor_default_url = '';
+clicksor_banner_border = '#99CC33'; 
+clicksor_banner_ad_bg = '#FFFFFF';
+clicksor_banner_link_color = '#000000'; 
+clicksor_banner_text_color = '#666666';
+clicksor_layer_border_color = '';
+clicksor_layer_ad_bg = ''; 
+clicksor_layer_ad_link_color = '';
+clicksor_layer_ad_text_color = ''; 
+clicksor_text_link_bg = '';
+clicksor_text_link_color = ''; 
+clicksor_enable_text_link = false;
+		 
+clicksor_banner_text_banner = true;
+clicksor_banner_image_banner = true; 
+clicksor_enable_layer_pop = false;
+clicksor_enable_pop = true;
+</script>
+ <script type="text/javascript" src="http://b.clicksor.net/show.php?nid=1&amp;pid=392679&amp;adtype=&amp;sid=655850"></script>
+
+<script type="text/javascript"> 
+//default pop-under house ad url 
+clicksor_enable_pop = true; 
+clicksor_adhere_opt='left'; 
+clicksor_enable_text_link =true;
+durl = '';
+clicksor_layer_border_color = '';
+clicksor_layer_ad_bg = '';
+clicksor_layer_ad_link_color = '';
+clicksor_layer_ad_text_color = '';
+clicksor_text_link_bg = '';
+clicksor_text_link_color = '';
+
+clicksor_enable_layer_pop = false;
+</script>
+ <script type="text/javascript" src="http://b.clicksor.net/show.php?nid=1&amp;pid=392679&amp;sid=655850"></script>
